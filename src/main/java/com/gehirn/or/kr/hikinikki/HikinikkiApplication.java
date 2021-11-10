e77da20d6e4d4b9c35219edfdec9645c98698ba9
cmsong <cmsong@shinasys.com>
2021-11-10 10:28:22 +0900

    init commit

---
+++ src/main/java/com/gehirn/or/kr/hikinikki/HikinikkiApplication.java
@@ -0,0 +1,13 @@
+package com.gehirn.or.kr.hikinikki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikinikkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikinikkiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gehirn/or/kr/hikinikki/HikinikkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gehirn.or.kr.hikinikki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikinikkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
